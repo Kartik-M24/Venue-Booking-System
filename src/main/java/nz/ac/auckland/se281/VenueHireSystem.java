665c9fdbb489f48e12ea37185846d57b41247801
Kartik Malik <kmal768@aucklanduni.ac.nz>
2024-04-07 13:29:21 +1200

    Added section to check if booking date is prior to system date via .split()

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -68,10 +68,14 @@ public class VenueHireSystem {
   }
 
   public void makeBooking(String[] options) {
-    // Pre-setting variables
+    // Pre-setting and initialising variables
     String venueCode = options[0];
+    String venueDate = options[1];
     numberOfVenues = hireVenue.size();
     boolean venueCodeExists = false;
+    int systemDay;
+    int systemMonth;
+    int systemYear;
 
     for (int i = 0; i < numberOfVenues; i++) {
       if (hireVenue.get(i).getVenueCode().equals(venueCode)) {
@@ -79,9 +83,28 @@ public class VenueHireSystem {
       }
     }
 
+    // splitting the venue date into day, month, and year
+    String[] dateSplit = venueDate.split("/");
+    int day = Integer.parseInt(dateSplit[0]);
+    int month = Integer.parseInt(dateSplit[1]);
+    int year = Integer.parseInt(dateSplit[2]);
+
     // Outputting error message if booking conditions aren't met
     if (this.date == null) {
       MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
+    } else {
+      // splitting the system date into day, month, and year (after confirming system date is set)
+      String[] systemDateSplit = this.date.split("/");
+      systemDay = Integer.parseInt(systemDateSplit[0]);
+      systemMonth = Integer.parseInt(systemDateSplit[1]);
+      systemYear = Integer.parseInt(systemDateSplit[2]);
+
+      // Check if the booking date is in the past
+      if (year < systemYear
+          || (year == systemYear && month < systemMonth)
+          || (year == systemYear && month == systemMonth && day < systemDay)) {
+        MessageCli.BOOKING_NOT_MADE_PAST_DATE.printMessage(venueDate, this.date);
+      }
     }
     if (numberOfVenues == 0) {
       MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
